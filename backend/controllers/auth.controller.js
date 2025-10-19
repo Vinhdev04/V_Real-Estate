ee2a66798b0c7a7d9da9590dfd9017bf832d357e
C.Vinh <pcv.fed@gmail.com>
2025-10-19 16:35:20 +0700

    feat(code): handle code login - create token with jsonwebtoken and cookieparser - part 02 [end]

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,9 @@
 import bcrypt from 'bcrypt';
 import prisma from '../library/prisma.lib.js';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
 // HANDLE REGISTER USER
 export const register = async(req,res) => {
     // get data from response
@@ -34,33 +38,63 @@ export const register = async(req,res) => {
 
 
 // HANDLE LOGIN USER
-export const login = async(req,res) => {
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-    try{
-        const {username,password} = req.body;
-        // check user exists
-        const user = await prisma.user.findFirst({
-            where: {username}
-        });
-
-        if(!user) res.status(404).json({message: 'User not found'});
-
-        // check password
-        const isPasswordCorrect = await bcrypt.compare(password,user.password);
-        if(!isPasswordCorrect) res.status(401).json({message: 'Password is incorrect'});
-
-        // login successful
-        res.status(200).json({message: 'Login successful'});
-
-        // create a new token
-    }catch(err){
-        console.log(err);
-        res.status(500).json({message: 'Failed to login user'});
+    // check user exists
+    const user = await prisma.user.findFirst({
+      where: { username },
+    });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
     }
-}
+
+    // check password
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: 'Password is incorrect' });
+    }
+
+    // create a new token
+    const timeExpire = 24 * 60 * 60; // 24 hours
+
+    // create a new token
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: timeExpire }
+    );
+
+    // set cookie
+    res.cookie('token', token, {
+      httpOnly: true,
+      maxAge: timeExpire * 1000,
+      sameSite: 'strict',
+      
+    });
+    // console.log(token);
+
+
+    // send final response
+    res.status(200).json({
+      message: 'Login successful',
+    //   token,
+    //   user: {
+    //     id: user.id,
+    //     username: user.username,
+    //   },
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Failed to login user' });
+  }
+};
+
 
 
 // HANDLE LOGOUT USER
 export const logout = async(req,res) => {
-    
+
 }
